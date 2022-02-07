0c5e1c15080750a89ae1a54eb35d4c9e2acc3b6b
clement <clement.cauliez@gmail.com>
2022-02-07 15:55:07 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express')
 var app = express()
+const port = process.env.PORT || 3000
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('lalalala')
 })
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Listening on port 3000...')
 })
